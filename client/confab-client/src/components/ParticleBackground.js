d0f6aabd05622492fc7cc521190d8e6f9e656777
Rakshan Sharma <rakshan793@gmail.com>
2021-07-02 04:20:22 +0530

    BG Color Update

--- client/confab-client/src/components/ParticleBackground.js
+++ client/confab-client/src/components/ParticleBackground.js
@@ -4,6 +4,6 @@ import ParticlesConfig from '../config/particle-config'
 
 export default function ParticlesBackground () {
     return (
-        <div style={{ backgroundColor: "#e6dadb", position: 'absolute', width: '100%', height: '100%', top: 0, bottom: 0, zIndex: -1 }}><Particles params={ParticlesConfig}></Particles></div>
+        <div style={{ backgroundColor: "#A6ABA5", position: 'absolute', width: '100%', height: '100%', top: 0, bottom: 0, zIndex: -1 }}><Particles params={ParticlesConfig}></Particles></div>
     )
 }
