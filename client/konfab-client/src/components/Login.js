3164c12bf23be00a5a63ef87305335d1dc103182
Rakshan Sharma <rakshan793@gmail.com>
2021-07-04 04:23:01 +0530

    Added Custon Anonymous Button

--- client/konfab-client/src/components/Login.js
+++ client/konfab-client/src/components/Login.js
@@ -8,6 +8,8 @@ import socialMediaAuth from '../service/auth'
 import GithubButton from 'react-github-login-button'
 import GoogleButton from 'react-google-button'
 import KonfabButton from './KonfabButton/KonfabButton'
+import AnonButton from './AnonButton/AnonButton'
+
 
 
 export default function Login() {
@@ -24,7 +26,7 @@ export default function Login() {
             <div class="row" style={{ padding: '5px' }}><KonfabButton onClick={console.log("Clicked")} type="dark" style={{ width: '100vw' }} label="Use Konfab ID (Login/Register)" /></div>  {/* Konfab Login Setup */}
             <div class="row" style={{ padding:'5px' }}><GithubButton onClick={() => handleOnClick(githubProvider)} type="dark" style={{ width:'100vw' }} label="Login with Github" /></div>
             <div class="row" style={{ padding: '5px' }}><GoogleButton onClick={() => handleOnClick(googleProvider)} type="dark" style={{ width: '100vw' }} label="Login with Google" /></div>
-            <div class="row" style={{ padding: '5px' }}><button onClick={() => handleOnClick('Anonymous')} class="btn btn-outline-light" style={{ width: '100vw' }}>Encrypted Anonymous Chatroom</button></div>
+            <div class="row" style={{ padding: '5px' }}><AnonButton onClick={() => handleOnClick('Anonymous')} type="dark" style={{ width: '100vw' }} label="Encrypted Anonymous Chatroom" /></div>
        </Container>
     )
 }
