2374f22b4fa5301b862dffd715ad04bc3177fcd3
Rakshan Sharma <rakshan793@gmail.com>
2021-07-16 01:22:43 +0530

    Added React Route Functionality

--- client/konfab-client/src/components/App.js
+++ client/konfab-client/src/components/App.js
@@ -3,14 +3,19 @@ import Login from './Login'
 import ParticleBackground from './ParticleBackground'
 import KonfabID_Login from './KonfabID_Login'
 import './reset.css'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      {/* <Login /> */}
-      <KonfabID_Login />
-      <ParticleBackground />
-   </div>
+    <Router>
+      <div>
+        <ParticleBackground />
+        <Switch>
+          <Route path='/' exact component={Login} />
+          <Route path='/KonfabID_Login' exact component={KonfabID_Login} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
